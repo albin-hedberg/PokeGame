53339d8a7db0251906bea7671de50ff85d8a5087
albin-hedberg <143542598+albin-hedberg@users.noreply.github.com>
2023-09-13 13:47:59 +0200

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,57 @@
+﻿using PokeGame;
+
+//// Här instansieras klassen Pokemon och ett objekt av typen Pokemon läggs i variabeln pikachu
+//// Detta görs med en konstruktor i klassen Pokemon som har 2 parametrar, name och type.
+//Pokemon pikachu = new Pokemon("Pikachu", PokeTypes.Electric);
+
+//// Här instansieras klassen Pokemon och ett objekt av typen Pokemon läggs i variabeln charmander
+//// Detta görs med en tom konstruktor och sedan tilldelas värden till Name och Type
+//Pokemon charmander = new Pokemon();
+//charmander.Name = "Charmander";
+//charmander.Type = PokeTypes.Fire;
+
+//// Här anropas metoden Attack på objektet charmander och en referens till objektet pikachu skickas in som argument
+//charmander.Attack(pikachu);
+//// Här anropas metoden Attack på objektet pikachu och en referens till objektet charmander skickas in som argument
+//pikachu.Attack(charmander);
+
+//// Här skrivs värdet på propertyn HealthPoints ut för båda objekten pikachu och charmander
+//Console.WriteLine("Pikachu health points: " + pikachu.HealthPoints);
+//Console.WriteLine("Charmander health points: " + charmander.HealthPoints);
+
+////Console.WriteLine(pikachu.Name);
+////Console.WriteLine(pikachu.Type);
+////Console.WriteLine(charmander.Name);
+////Console.WriteLine(charmander.Type);
+
+////charmander.Type = "tjoho0";
+
+////Console.WriteLine(charmander.Name);
+////Console.WriteLine(charmander.Type);
+
+Trainer ash = new Trainer();
+
+Pokemon pikachu = new Pokemon("Pikachu", PokeTypes.Electric);
+Pokemon charmander = new Pokemon("Charmander", PokeTypes.Fire);
+Pokemon squirtle = new Pokemon("Squirtle", PokeTypes.Water);
+Pokemon bulbasur = new Pokemon("Bulbasur", PokeTypes.Grass);
+Pokemon mew = new Pokemon("Mew", PokeTypes.Psychic);
+
+ash.Catch(pikachu);
+ash.Catch(charmander);
+ash.Catch(squirtle);
+ash.Catch(bulbasur);
+ash.Catch(mew);
+
+foreach (Pokemon pokemon in ash.PokemonList)
+{
+    Console.WriteLine(pokemon.Name);
+}
+
+Console.WriteLine("-------------------------");
+
+for (int i = 0; i < ash.PokemonList.Count;)
+{
+    Console.WriteLine($"{ash.PokemonList[0].Name} was released!");
+    ash.Release(ash.PokemonList[0]);
+}
---
+++ ConsoleApp2/Trainer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokeGame
+{
+    internal class Trainer
+    {
+        List<Pokemon> _pokemonList = new List<Pokemon>();
+        public List<Pokemon> PokemonList
+        {
+            get { return _pokemonList; }
+            set { _pokemonList = value; }
+        }
+
+        public void Catch(Pokemon pokemon)
+        {
+            _pokemonList.Add(pokemon);
+        }
+
+        public void Release(Pokemon pokemon)
+        {
+            if (_pokemonList.Contains(pokemon))
+            {
+                _pokemonList.Remove(pokemon);
+            }
+        }
+
+        // Detta är en statisk metod. Statiska metoder anropas via typen och inte via objekt.
+        public static void Greeting() 
+        {
+            Console.WriteLine("Hi!");
+        }
+    }
+}
